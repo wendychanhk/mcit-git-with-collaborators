6437de04c37d48f9284cd6024e02302c36f72730
Benjamin Barba <bbarba@imbellus.com>
2019-06-17 18:25:14 -0700

    Changes to println

--- test.java
+++ test.java
@@ -1,5 +1,6 @@
 public class Test {
 	public static void main(String[] args) {
-    System.out.println("Git collaborators exercise"));
+    System.out.print("Git collaborators exercise - changed println to print"));
+	System.out.println("let me know if you need me to make any further changes!"));
 	}
 }
